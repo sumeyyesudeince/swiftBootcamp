640e437973b86af9b737dfb63e55936882c3f39e
Sümeyye Sude İnce <80282830+sumeyyesudeince@users.noreply.github.com>
2024-10-10 13:04:55 +0300

    Add files via upload

---
+++ Odev4/Odev4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev4
+//
+//  Created by Sude İnce on 10.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
